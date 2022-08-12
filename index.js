9f1d90ced62c8fc11e97ca9d274ce4031c3b3cae
pronepoet <ochiengphillip84@gmail.com>
2022-08-12 08:58:40 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+let names = ["Ada", "Brendan", "Ali"];
+function writeCards(names, event){
+  let messages = [];
+  for(let i = 0; i<names.length; i++){
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+}
+function countDown(number){
+  let i = number;
+  while (i>=0){
+    console.log(i--);
+  }
+}
